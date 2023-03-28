c088f27ca0f8f6e33a1847fa3126b18bd561baa6
Jingye Xu <jingye.xu@outlook.com>
2023-03-28 00:41:14 -0500

    update test script

--- test_REST.py
+++ test_REST.py
@@ -1,11 +1,20 @@
 import requests
 
-post_data = {
+job_post_data = {
     "username": "test1",
     "passwd": "test2",
     "text": "please show me the right msg."
 }
 
-data = requests.post("http://172.16.0.133:5000/job", json=post_data)
+job_get_data = {
+    "id": 123456
+}
+
+data = requests.post("http://172.16.0.133:5000/job", json=job_post_data)
+
+print(data.json())
+
+
+data = requests.get("http://172.16.0.133:5000/job", json=job_get_data)
 
 print(data.json())
